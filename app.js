1f4c96041537e1967d9123d1cd23a812eb203ee9
Your Name <you@example.com>
2019-05-16 01:08:13 -0400

    json data conversion and public build

--- app.js
+++ app.js
@@ -6,7 +6,21 @@ const port = process.env.PORT || 3000;
 
 // app
 const app = express()
+app.use(bodyParser.json())
+app.use(express.static('public'))
 
+// mongoDB
+mongoose.connect('mongodb://localhost:27017/dictionary', {useNewUrlParser: true},
+// if statement function checking if there's a connection or error
+function (error) {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log("connected")
+    }
+});
+
+// port listening
 app.listen(port, () => console.log(`App listening on port ${port}`));
 
 module.exports = app;
---
+++ public/script.js
@@ -0,0 +1,18 @@
+function handleClick() {
+    console.log(" button click successful!")
+
+    const input = document.getElementById("input").value;
+
+    const url = './data?input=' + input;
+
+    axios.get(url)
+        .then (res => {
+            console.log(res)
+                if (res.data.length > 0) {
+                    const returnedData = res.data[0].word
+                    document.getElementById('results').innerHTML = returnedData + " word exists"
+                } else {
+                    document.getElementById('results').innerHTML = "word does not exist"
+                }
+        })
+}
